ed49eff9a0e217ada47cc282adbb203ef177603e
NexCreep <61358860+NexCreep@users.noreply.github.com>
2021-01-11 14:39:05 +0100

    First box created + fixed in top navbar

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,17 @@
 $(document).ready(()=>{
     $("#ScrollB").click(()=>{
         $("html, body").animate({
-            scrollTop: $("#bye").offset().top
+            scrollTop: $("#box-a").offset().top - 200
         }, 500)
     });
     $("#ScrollA").click(()=>{
         $("html, body").animate({
             scrollTop: $("#hello").offset().top
         }, 500)
-    })
+    });
+    $(window).scroll(()=>{
+        
+    });
+    $("#box-a").hide()
+        .fadeIn(1000);
 })
